919b9cff284331c275eda07ccb6d7f31e96ecec7
Alisher Sadikov <asadikov@msts.com>
2018-04-12 21:43:53 -0500

    Complete kata_04

---
+++ kata_04/kata_04_temp_spread.rb
@@ -0,0 +1,38 @@
+class TempSpread
+  attr_reader :file, :spreads_by_day
+  attr_accessor :headers
+
+  def initialize(file)
+    @file = file
+    @headers ||= []
+    @spreads_by_day ||= {}
+  end
+
+  def calculate_max_spread
+    calculate_spreads # populate the spreads
+    # sorts by the spread ascending, gets the last element, spits out the day
+    spreads_by_day.sort_by { |day| day[1]["Spread"] }.last.first
+  end
+
+  private
+
+    def parse(file)
+      # Open the file, read it, map the lines by grabbing the first 14 chars
+      File.open(file, File::RDONLY){|f| f.read }.lines.map { |line| line[0..13].split }
+    end
+
+    def calculate_spreads
+      prepared_data.each do |line|
+        spreads_by_day[line[0]] = { headers[1] => line[1],
+                                    headers[2] => line[2],
+                                    "Spread" => (line[1].to_i - line[2].to_i).abs }
+      end
+    end
+
+    def prepared_data
+      raw_data = parse(file)
+      self.headers = raw_data.shift # stores and gets rid of the header array
+      raw_data.shift # gets rid of the empty array
+      raw_data
+    end
+end
---
+++ kata_04/kata_04_temp_spread_test.rb
@@ -0,0 +1,29 @@
+require 'minitest/autorun'
+require_relative 'kata_04_temp_spread'
+
+class TempSpreadTest < Minitest::Test
+  attr_reader :obj
+
+  def setup
+    @obj = TempSpread.new('./weather.dat')
+  end
+
+  def test_initial_state_of_attributes
+    assert obj.file
+    assert_equal [], obj.headers
+    assert obj.spreads_by_day == {}
+  end
+
+  def test_single_day_spreads
+    obj.calculate_max_spread # to populate everything
+    refute_equal [], obj.headers
+    refute_empty obj.spreads_by_day
+
+    assert_equal 29, obj.spreads_by_day["1"]["Spread"]
+    assert_equal 16, obj.spreads_by_day["2"]["Spread"]
+  end
+
+  def test_max_spread
+    assert_equal '9', obj.calculate_max_spread
+  end
+end
